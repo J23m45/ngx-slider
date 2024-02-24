b1e18f91fe0d23699e6676ba03acb83773eea991
Devon Wesley <dwesley@trialogics.com>
2024-02-24 01:48:28 -0500

    test

--- src/ngx-slider/lib/value-helper.ts
+++ src/ngx-slider/lib/value-helper.ts
@@ -1,4 +1,4 @@
-import { CustomStepDefinition } from './options';
+import { CustomStepDefinition } from "./options";
 
 /**
  *  Collection of functions to handle conversions/lookups of values
@@ -22,12 +22,20 @@ export class ValueHelper {
     return true;
   }
 
-  static linearValueToPosition(val: number, minVal: number, maxVal: number): number {
+  static linearValueToPosition(
+    val: number,
+    minVal: number,
+    maxVal: number
+  ): number {
     const range: number = maxVal - minVal;
     return (val - minVal) / range;
   }
 
-  static logValueToPosition(val: number, minVal: number, maxVal: number): number {
+  static logValueToPosition(
+    val: number,
+    minVal: number,
+    maxVal: number
+  ): number {
     val = Math.log(val);
     minVal = Math.log(minVal);
     maxVal = Math.log(maxVal);
@@ -35,23 +43,39 @@ export class ValueHelper {
     return (val - minVal) / range;
   }
 
-  static linearPositionToValue(percent: number, minVal: number, maxVal: number): number {
+  static linearPositionToValue(
+    percent: number,
+    minVal: number,
+    maxVal: number
+  ): number {
     return percent * (maxVal - minVal) + minVal;
   }
 
-  static logPositionToValue(percent: number, minVal: number, maxVal: number): number {
+  static logPositionToValue(
+    percent: number,
+    minVal: number,
+    maxVal: number
+  ): number {
     minVal = Math.log(minVal);
     maxVal = Math.log(maxVal);
     const value: number = percent * (maxVal - minVal) + minVal;
     return Math.exp(value);
   }
 
-  static findStepIndex(modelValue: number, stepsArray: CustomStepDefinition[]): number {
-    const differences: number[] = stepsArray.map((step: CustomStepDefinition): number => Math.abs(modelValue - step.value));
+  static findStepIndex(
+    modelValue: number,
+    stepsArray: CustomStepDefinition[]
+  ): number {
+    const differences: number[] = stepsArray.map(
+      (step: CustomStepDefinition): number => Math.abs(modelValue - step.value)
+    );
 
     let minDifferenceIndex: number = 0;
     for (let index: number = 0; index < stepsArray.length; index++) {
-      if (differences[index] !== differences[minDifferenceIndex] && differences[index] < differences[minDifferenceIndex]) {
+      if (
+        differences[index] !== differences[minDifferenceIndex] &&
+        differences[index] < differences[minDifferenceIndex]
+      ) {
         minDifferenceIndex = index;
       }
     }
