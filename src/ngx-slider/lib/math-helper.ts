965d33aa5792e0dee6ded33e8ee68abbd55117b7
Devon Wesley <dwesley@trialogics.com>
2024-02-25 16:33:10 -0500

    test

--- src/ngx-slider/lib/math-helper.ts
+++ src/ngx-slider/lib/math-helper.ts
@@ -1,13 +1,20 @@
-/** Helper with mathematical functions */
+/** Helper with mathematical functions  */
 export class MathHelper {
   /* Round numbers to a given number of significant digits */
   static roundToPrecisionLimit(value: number, precisionLimit: number): number {
-    return +( value.toPrecision(precisionLimit) );
+    return +value.toPrecision(precisionLimit);
   }
 
-  static isModuloWithinPrecisionLimit(value: number, modulo: number, precisionLimit: number): boolean {
+  static isModuloWithinPrecisionLimit(
+    value: number,
+    modulo: number,
+    precisionLimit: number
+  ): boolean {
     const limit: number = Math.pow(10, -precisionLimit);
-    return Math.abs(value % modulo) <= limit || Math.abs(Math.abs(value % modulo) - modulo) <= limit;
+    return (
+      Math.abs(value % modulo) <= limit ||
+      Math.abs(Math.abs(value % modulo) - modulo) <= limit
+    );
   }
 
   static clampToRange(value: number, floor: number, ceil: number): number {
